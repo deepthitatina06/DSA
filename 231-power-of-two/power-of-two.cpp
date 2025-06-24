1660839409facfd9d61cea076777a4bd9a7553e0
deepthitatina06 <22P31A04I4@acet.ac.in>
2025-06-24 20:27:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (83.94%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0)return false;
+         if(n<=0)return false;
         return(n&(n-1))==0;
     
     }
@@ -11,7 +11,8 @@ public:
         cout<<"enter base and power is";
         cin>>x;
         bool res=isPowerOfTwo(x);
-        cout<<"the result is:"<<(res?"true":"false")<<endl;
+        cout<<"the result is:"<<res<<"true"<<"false"<<endl;
         return 0;
+        
     }
 };
